c7981a228760de817f30fc8c3ffff6085dcc0951
wayneq <supreme_wayne@163.com>
2023-09-06 12:41:26 +0800

    ...

--- src/components/Beats.vue
+++ src/components/Beats.vue
@@ -7,15 +7,14 @@ defineProps<{
 </script>
 <template>
     <div>
-        <button v-for="(beat, i) in beats" :key="i" class="btn btn-lg mx-1 px-2 text-center" style="transition: none;"
-            :class="i === curBeat ? 'bg-success' : 'bg-dark'" @click="beat.mute = !beat.mute">
-            <svg :class="beat.mute ? '' : 'invisible'" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
+        <button v-for="(beat, i) in beats" :key="i" class="btn mx-1 p-0"
+            style="transition: none;border-radius: 50%; width:3em;height:3em"
+            :class="i === curBeat ? 'btn-success' : 'btn-dark'" @click="beat.mute = !beat.mute">
+            <svg :class="beat.mute ? '' : 'invisible'" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                 fill="currentColor" viewBox="0 0 16 16">
                 <path
-                    d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z" />
+                    d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
             </svg>
-            <br>
-            {{ i + 1 }}
         </button>
     </div>
 </template>
